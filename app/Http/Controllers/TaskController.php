0b71a0e7bbf6d198ceb19d88c110aa6bfd7ae446
Daiki <d_shiozawa@yumemi.co.jp>
2021-03-15 17:06:35 +0900

    Add: Taskテーブル作った

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Folder;
+use App\Models\Task;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
@@ -10,9 +11,13 @@ class TaskController extends Controller
     public function index(int $id) {
         $folders = Folder::all();
 
+        $current_folder = Folder::find($id);
+        $tasks = Task::where('folder_id', $current_folder->id)->get();
+
         return view('tasks/index', [
             'folders' => $folders,
             'current_folder_id' => $id,
+            'tasks' => $tasks,
         ]);
     }
 }
