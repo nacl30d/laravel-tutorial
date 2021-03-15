d740cee0233ad5d1868c7c33ca8220727da04ae3
Daiki <d_shiozawa@yumemi.co.jp>
2021-03-15 12:05:18 +0900

    Update: Factoryをコメントアウト

--- app/Models/Folder.php
+++ app/Models/Folder.php
@@ -2,10 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+// use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Folder extends Model
 {
-    use HasFactory;
+    // use HasFactory;
 }
