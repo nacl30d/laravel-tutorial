1daa99d74e83fe4d23fc4d10d36c042bd83089a8
Daiki <d_shiozawa@yumemi.co.jp>
2021-03-29 13:41:52 +0900

    Fix: 吹っ飛んだルーティングをサルベージ

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\TaskController;
+use App\Http\Controllers\FolderController;
+use App\Http\Controllers\HomeController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,9 +17,45 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get(
+    '/',
+    [HomeController::class, 'index']
+)->name('home');
+
+Route::get(
+    '/folders/create',
+    [FolderController::class, 'showCreateForm']
+)->name('folders.create');
+
+Route::post(
+    '/folders/create',
+    [FolderController::class, 'create']
+);
+
+Route::get(
+    '/folders/{id}/tasks',
+    [TaskController::class, 'index']
+)->name('tasks.index');
+
+Route::get(
+    '/folders/{id}/tasks/create',
+    [TaskController::class, 'showCreateForm']
+)->name('tasks.create');
+
+Route::post(
+    '/folders/{id}/tasks/create',
+    [TaskController::class, 'create']
+);
+
+Route::get(
+    '/folders/{id}/tasks/{task_id}/edit',
+    [TaskController::class, 'showEditForm']
+)->name('tasks.edit');
+
+Route::post(
+    '/folders/{id}/tasks/{task_id}/edit',
+    [TaskController::class, 'edit']
+);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
