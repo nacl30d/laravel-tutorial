9f8e8439be4320362950f350df2a49f4396e5a65
Daiki <d_shiozawa@yumemi.co.jp>
2021-03-29 13:24:57 +0900

    Add: 認証機能をインストールした

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\TaskController;
-use App\Http\Controllers\FolderController;
-use App\Http\Controllers\HomeController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,42 +13,12 @@ use App\Http\Controllers\HomeController;
 |
 */
 
-Route::get(
-    '/',
-    [HomeController::class, 'index']
-)->name('home');
+Route::get('/', function () {
+    return view('welcome');
+});
 
-Route::get(
-    '/folders/create',
-    [FolderController::class, 'showCreateForm']
-)->name('folders.create');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
-Route::post(
-    '/folders/create',
-    [FolderController::class, 'create']
-);
-
-Route::get(
-    '/folders/{id}/tasks',
-    [TaskController::class, 'index']
-)->name('tasks.index');
-
-Route::get(
-    '/folders/{id}/tasks/create',
-    [TaskController::class, 'showCreateForm']
-)->name('tasks.create');
-
-Route::post(
-    '/folders/{id}/tasks/create',
-    [TaskController::class, 'create']
-);
-
-Route::get(
-    '/folders/{id}/tasks/{task_id}/edit',
-    [TaskController::class, 'showEditForm']
-)->name('tasks.edit');
-
-Route::post(
-    '/folders/{id}/tasks/{task_id}/edit',
-    [TaskController::class, 'edit']
-);
+require __DIR__.'/auth.php';
